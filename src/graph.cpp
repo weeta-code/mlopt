2c18c71df5522d7e7ccdbe92ffb702681240441d
weeta-code <victordesouza132@gmail.com>
2025-09-30 22:54:23 -0400

    feat: working implementation, skeleton cpp

--- src/graph.cpp
+++ src/graph.cpp
@@ -23,11 +23,29 @@ NodeId GraphModule::add_node(const std::string& op, const std::vector<ValueId>& 
   return NodeId{0};
 }
 
-ValueId GraphModule::add_output(ValueId v) { return v; }
+ValueId GraphModule::add_output(ValueId v) { return v; } // revisit, should mark existing value as graph output?
 
 ValueId GraphModule::add_const_scalar(DType, const AttrValue&) {
   // TODO : 
 
   return ValueId{0};
 }
+
+std::vector<NodeId> GraphModule::nodes() const { return {}; }
+std::vector<NodeId> GraphModule::inputs() const { return {}; }
+std::vector<NodeId> GraphModule::outputs() const { return {}; }
+
+NodeView GraphModule::get_node(NodeId) const { return {}; }
+ValueView GraphModule::get_value(ValueId) const { return {}; }
+
+size_t GraphModule::num_nodes() const { return 0; }
+size_t GraphModule::num_values() const { return 0; }
+
+Status GraphModule::replace_node(NodeId node, const std::string& new_op, 
+                                 const std::vector<ValueId>& new_inputs,
+                                 const AttrMap& new_attrs, NodeId*) {
+  return Status::OK();
+}
+
+Status GraphModule::topological_sort(std::vector<NodeId>* out) const;
 }
